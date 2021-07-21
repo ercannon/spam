0f2f1a0eaf15f073bc878fd29f8bd98a4fb22494
ercannon <87174255+ercannon@users.noreply.github.com>
2021-07-21 17:45:13 +0200

    creacion del proyecto

---
+++ fetcher/fetch_data.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 21 17:43:11 2021
+
+@author: cannon
+"""
---
+++ main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 21 17:42:56 2021
+
+@author: cannon
+"""
