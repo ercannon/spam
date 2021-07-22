32f436aa4ebb16e7b8a55d4e8e9a118fd24c0be0
ercannon <87174255+ercannon@users.noreply.github.com>
2021-07-22 16:21:28 +0200

    anyadidos los modelos

--- datamanagement/data_split.py
+++ datamanagement/data_split.py
@@ -5,3 +5,15 @@ Created on Wed Jul 21 19:00:44 2021
 
 @author: cannon
 """
+
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+
+def split_data(ham_emails, spam_emails):
+    X = np.array(ham_emails + spam_emails, dtype=object)
+    y = np.array([0] * len(ham_emails) + [1] * len(spam_emails))
+    
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    return X_train, X_test, y_train, y_test
--- main.py
+++ main.py
@@ -9,7 +9,29 @@ Created on Wed Jul 21 17:42:56 2021
 from fetcher.fetch_data import fetch_data
 from datamanagement.analyse_data import load_mails
 from datamanagement.analyse_data import structures_counter
+from datamanagement.data_split import split_data
+from datamanagement.clean_data import preprocess_data
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import precision_score, recall_score
+
 
 #fetch_data()
 ham_emails,spam_emails=load_mails()
-print(structures_counter(ham_emails).most_common())
+#print(structures_counter(ham_emails).most_common())
+x_train, x_test, y_train, y_test = split_data(ham_emails, spam_emails)
+x_train_transformed = preprocess_data(x_train)
+log_clf = LogisticRegression(solver="lbfgs", max_iter=1000, random_state=42)
+score = cross_val_score(log_clf, x_train_transformed, y_train, cv=3, verbose=3)
+score.mean()
+
+x_test_transformed = preprocess_data(x_test)
+log_clf = LogisticRegression(solver="lbfgs", max_iter=1000, random_state=42)
+log_clf.fit(x_train_transformed, y_train)
+
+y_pred = log_clf.predict(x_test_transformed)
+
+print("Precision: {:.2f}%".format(100 * precision_score(y_test, y_pred)))
+print("Recall: {:.2f}%".format(100 * recall_score(y_test, y_pred)))
+
+
